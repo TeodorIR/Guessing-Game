e9dc21d1bda8a9a46b95d9c54875505f2de5dc82
Teodor <raduteodorionut@gmail.com>
2024-10-10 16:14:53 +0300

    created game

---
+++ game.js
@@ -0,0 +1,35 @@
+/* game.js
+This is the JavaScript file for our guessing game. */
+
+"use strict";
+
+window.onerror = function (message, url, line) {
+  alert(`Error: ${message}\n${url}: ${line}`);
+};
+
+init();
+function init() {
+  let button = document.querySelector("button#startgame");
+  button.onclick = playGame;
+}
+
+/* function playGame() {
+Set up the data
+
+Initialise
+
+Repeat
+   User guesses
+   Check and report
+
+...until correct
+} */
+
+function playGame() {
+  let answer, guess;
+  answer = 57;
+
+  do {
+    guess = prompt("Guess the number between 1 and 100");
+  } while (guess != answer);
+}
